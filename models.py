ce0f74691df79de25898fc957de0e081d3fcf20a
Urs de Swardt <urs-johann.de-swardt@hpe.com>
2025-02-09 21:00:47 +0200

    add examples

--- models.py
+++ models.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from sqlmodel import Field, SQLModel
+from settings import settings
 
 
 class SensorData(SQLModel, table=True):
-    __tablename__ = "sensor_data"
+    __tablename__ = settings.TABLE_NAME
 
     id: int | None = Field(default=None, primary_key=True)
     temperature: float
--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 
-class Settings(BaseSettings):
+class SqliteCloudSettings(BaseSettings):
     DATABASE_NAME: str = "your-database-name"
     PROJECT_ID: str = "your-project-id"
     API_KEY: str = "your-api-key"
@@ -9,6 +9,14 @@ class Settings(BaseSettings):
     @property
     def sqlite_cloud_url(self) -> str:
         return f"sqlitecloud://{self.PROJECT_ID}.sqlite.cloud:8860/{self.DATABASE_NAME}?apikey={self.API_KEY}"
+
+
+class ExampleSettings(BaseSettings):
+    TABLE_NAME: str = "sensor_data"
+
+
+class Settings(SqliteCloudSettings, ExampleSettings):
+    pass
 
 
 settings = Settings()
